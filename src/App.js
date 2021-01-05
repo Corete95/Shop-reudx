658c3b4f73b468f376f98ae3348a73a507458f8f
Corete <fanthell95@gmail.com>
2021-01-05 15:09:09 +0900

    상품리스트 컴포넌트Map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
+import React, { useState } from "react";
 import "./App.css";
+import { Navbar, Nav, NavDropdown, Jumbotron, Button } from "react-bootstrap";
+import data from "./data.js";
 
 function App() {
-  return <div className="App">asd</div>;
+  let [shoes, setShoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg" className="Navbar">
+        <Navbar.Brand href="#home">신발 구멍가게</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Jumbotron className="center">
+        <h1>60% 시즌 세일!!!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+      <div className="container">
+        <div className="row">
+          {shoes.map((shoes, idx) => {
+            return <ShoesList shoes={shoes} idx={idx} key={idx} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ShoesList(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.idx + 1) + ".jpg"
+        }
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
+    </div>
+  );
 }
 
 export default App;
